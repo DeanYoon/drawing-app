e73e08367c79b22cec968017e2eec6881a1f3199
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2022-10-30 14:41:35 +0900

    button css added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const sizePen = document.querySelector(".size-pen");
+const sizeWord = document.querySelector(".size-word");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
+const navBtns = Array.from(document.getElementsByClassName("button"));
 const color = document.querySelector("#color");
 const colorForSelect = document.querySelector(".select-color");
 const penBtn = document.querySelector(".pen");
@@ -18,6 +21,8 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = 400;
 const CANVAS_HEIGHT = 300;
 
+const HIDE_CLASS = "hidden";
+const CLICKED = "clicked";
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
@@ -26,6 +31,7 @@ let font = 30;
 let isPainting = false;
 let isFilling = false;
 let isStamp = false;
+let nowColor = "black";
 
 function reset() {
   isPainting = false;
@@ -33,6 +39,9 @@ function reset() {
   isStamp = false;
 }
 
+function unBtnAll() {
+  navBtns.forEach((button) => button.classList.remove(CLICKED));
+}
 //when stamp is on, don't paint
 function onMouseDown() {
   if (isStamp === false) {
@@ -60,7 +69,7 @@ function lineSize(event) {
     //Only when stamp button was clicked
     if (event.deltaY < 0) {
       font += 1;
-    } else {
+    } else if (font > 4) {
       font -= 1;
     }
   } else {
@@ -84,30 +93,48 @@ function stampCanvas(event) {
 
 function onPenBtnClick() {
   reset();
+  unBtnAll();
+  ctx.strokeStyle = nowColor;
+  penBtn.classList.add(CLICKED);
   isPainting = true;
+  sizePen.classList.remove(HIDE_CLASS);
+  sizeWord.classList.add(HIDE_CLASS);
 }
 function onPaintClick() {
   reset();
+  unBtnAll();
+  ctx.strokeStyle = nowColor;
+  paintBtn.classList.add(CLICKED);
   isFilling = true;
 }
 function onEraserBtnClick() {
   reset();
+  unBtnAll();
+  eraserBtn.classList.add(CLICKED);
   isPainting = true;
   ctx.strokeStyle = "white";
 }
 function onTrashBtnClick() {
+  unBtnAll();
+  trashBtn.classList.add(CLICKED);
   ctx.save();
   ctx.fillStyle = "white";
-  ctx.fillRect(5, 5, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   ctx.restore();
 }
 
 function onStampBtnClick() {
+  unBtnAll();
+  stampBtn.classList.add(CLICKED);
   reset();
   isStamp = true;
+  sizePen.classList.add(HIDE_CLASS);
+  sizeWord.classList.remove(HIDE_CLASS);
 }
 
 function onUploadChanged(event) {
+  unBtnAll();
+  uploadBtn.classList.add(CLICKED);
   const file = event.target.files[0];
   const url = URL.createObjectURL(file);
   const image = new Image();
@@ -118,6 +145,8 @@ function onUploadChanged(event) {
 }
 
 function onDownloadBtnClick() {
+  unBtnAll();
+  downloadBtn.classList.add(CLICKED);
   const url = canvas.toDataURL();
   const a = document.createElement("a");
   a.href = url;
@@ -129,12 +158,22 @@ function onColorClick(event) {
   const colorValue = event.target.dataset.color;
   ctx.strokeStyle = colorValue;
   ctx.fillStyle = colorValue;
+  nowColor = colorValue;
 }
+
 function onColorChange(event) {
   const selectColor = event.target.value;
   ctx.strokeStyle = selectColor;
   ctx.fillStyle = selectColor;
-  colorForSelect.style.backgroundColor = selectColor;
+  colorForSelect.style.background = selectColor;
+}
+function getSize() {
+  getLineWidth = ctx.lineWidth;
+  sizePen.style.width = `${getLineWidth}px`;
+  sizePen.style.height = `${getLineWidth}px`;
+  sizePen.style.background = nowColor;
+  sizeWord.style.fontSize = `${font}px`;
+  sizeWord.style.color = nowColor;
 }
 
 canvas.addEventListener("mousedown", onMouseDown);
@@ -155,3 +194,8 @@ downloadBtn.addEventListener("click", onDownloadBtnClick);
 
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 color.addEventListener("change", onColorChange);
+
+getSize();
+setInterval(getSize, 10);
+
+console.log(navBtns);
